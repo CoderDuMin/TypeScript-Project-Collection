c6021e9eea06218b94da6fc0ec07d1c5d9a04582
CoderDuMin <1325964835@qq.com>
2023-04-19 20:54:43 +0800

    饿了么ui库引入

--- CMS-Vue3-TS/src/main.ts
+++ CMS-Vue3-TS/src/main.ts
@@ -1,9 +1,17 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 
+// 1.全局引入
+// import ElementPlus from "element-plus"
+import "element-plus/dist/index.css"
+// app.use(ElementPlus)
+
 import "normalize.css"
 import "./assets/main.css"
 import pinia from "./store"
 import router from "./router"
 
-createApp(App).use(pinia).use(router).mount("#app")
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
+app.mount("#app")
